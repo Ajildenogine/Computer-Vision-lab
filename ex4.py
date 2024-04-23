af9ce641a40577fe235955b1d537947cfffff1be
Maheshwar Muthukumar <101420124+zealousMW@users.noreply.github.com>
2024-04-24 03:34:27 +0530

    Update ex4.py

--- ex4.py
+++ ex4.py
@@ -9,21 +9,24 @@ hslimg = cv2.cvtColor(img, cv2.COLOR_BGR2HLS )
 equ = cv2.equalizeHist(greyimg)
 cv2.imshow(" ",equ)
 cv2.waitKey(0)
-
 cv2.imshow(" ",greyimg)
 cv2.waitKey(0)
-
 cv2.imshow(" ",rbgimg)
 cv2.waitKey(0)
-
 cv2.imshow(" ",hslimg)
 cv2.waitKey(0)
-
 imgblur = cv2.blur(img,(5,5))
 cv2.imshow(" ",imgblur)
 cv2.waitKey(0)
+kernel = np.ones((3,3), np.float32) / 9
+
+
+convolved_image = cv2.filter2D(img, -1, kernel)
+cv2.imshow(" ",convolved_image)
+cv2.waitKey(0)
 
 imgblur = cv2.GaussianBlur(img,(5,5),0,0)
+
 cv2.imshow(" ",imgblur)
 cv2.waitKey(0)
 
@@ -48,3 +51,6 @@ cv2.waitKey(0)
 combin = cv2.addWeighted(gx , 0.5 , gy , 0.5, 0)
 cv2.imshow(" ",combin)
 cv2.waitKey(0)
+
+edges = cv2.Canny(greyimg, 100, 200)
+cv2.imshow(" ", edges)
